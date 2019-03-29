86835cda40a6adb14edac5af739357daa705e10d
zf <413273029@qq.com>
2019-03-29 17:53:21 +0800

    更新 song.html 显示逻辑

--- song.js
+++ song.js
@@ -10,60 +10,73 @@ $(function () {
       return s.id == id
     })[0]
     let {
-      url
-    } = songId
-    console.log(url);
-    let audio = document.createElement('audio')
-    audio.src = url
-    audio.setAttribute('muted', 'muted')
-    audio.oncanplay = function () {
-      $('.disc-container').addClass('playing')
-      audio.play()
-
-    }
-    $('.icon-pause').on('touchstart', () => {
-      audio.pause()
-      $('.disc-container').removeClass('playing')
-    })
-
-    $('.icon-play').on('touchstart', () => {
-      audio.play()
-      $('.disc-container').addClass('playing')
-    })
-
-
-  })
-
-  $.get('/lyric.json').then(function (object) {
-    let {
+      url,
+      name,
       lyric
-    } = object
-    let array = lyric.split('\n')
-    let regex = /^\[(.+)\](.*)$/
-    array = array.map(function (string) {
-      let matches = string.match(regex)
-      if (matches) {
-        return {
-          time: matches[1],
-          words: matches[2]
-        }
-      }
-    })
-    let $lyric = $('.lyric')
-    array.map(function (object) {
-      if (!object) {
-        return
-      }
-      let $p = $('<p/>')
-      $p.attr('data-time', object.time).text(object.words)
-      $p.appendTo($lyric.children('.line'))
-    })
+    } = songId
+    initPlayer.call(null, url)
+    initLyric.call(null, name, lyric) 
 
+    parseLyric.call(null, lyric)
   })
 
 
-})
+//   $.get('/lyric.json').then(function (object) {
+//     let {
+//       lyric
+//     } = object
+    
+// })
 
+function initLyric(name, lyric) {
+  $('.song-description h1').text(name)
+}
+
+function parseLyric(lyric) {
+  let array = lyric.split('\n')
+  let regex = /^\[(.+)\](.*)$/
+  array = array.map(function (string) {
+    let matches = string.match(regex)
+    if (matches) {
+      return {
+        time: matches[1],
+        words: matches[2]
+      }
+    }
+  })
+  let $lyric = $('.lyric')
+  array.map(function (object) {
+    if (!object) {
+      return
+    }
+    let $p = $('<p/>')
+    $p.attr('data-time', object.time).text(object.words)
+    $p.appendTo($lyric.children('.line'))
+  })
+
+  
+}
+
+function initPlayer(url) {
+  let audio = document.createElement('audio')
+  audio.src = url
+  audio.setAttribute('muted', 'muted')
+  audio.oncanplay = function () {
+    $('.disc-container').addClass('playing')
+    audio.play()
+
+  }
+  $('.icon-pause').on('touchstart', () => {
+    audio.pause()
+    $('.disc-container').removeClass('playing')
+  })
+
+  $('.icon-play').on('touchstart', () => {
+    audio.play()
+    $('.disc-container').addClass('playing')
+  })
+}
+})
 
 
 
